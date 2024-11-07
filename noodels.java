2c617264c3972bd5dfcab85f5858407a6fdd8f1b
Rakshithasharma455 <rakaryan2002@gmail.com>
2024-11-07 18:55:22 +0530

    Add files via upload

---
+++ noodels.java
@@ -0,0 +1,18 @@
+package chef;
+
+import java.util.Scanner;
+
+public class noodels {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int x,y;
+		x=sc.nextInt();//the number of stoves
+		y=sc.nextInt();//the number of minutes
+		System.out.println(x*y);
+	
+		
+
+	}
+
+}
