dfc493180be7bae5438b99a0470eeed80bea4c8d
Rakshithasharma455 <rakaryan2002@gmail.com>
2024-11-07 19:02:08 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,23 @@
+package sumofcombination;
+
+import java.util.Scanner;
+
+public class sum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m;
+		int n;
+		int k;
+		m = sc.nextInt();
+		n = sc.nextInt();
+		k = sc.nextInt();
+		System.out.println(m+n);
+		System.out.println(m+k);
+		System.out.println(n+k);
+		
+		
+
+	}
+
+}
