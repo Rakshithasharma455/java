0f7eb491b7abf2c8c77104cf2bdeb94bdb9cc2c6
Rakshithasharma455 <rakaryan2002@gmail.com>
2024-11-07 18:53:45 +0530

    celsiustofahrenheit

---
+++ ctof.java
@@ -0,0 +1,15 @@
+package calciustofarenheit;
+
+import java.util.Scanner;
+
+public class ctof {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int m;//considering m as celsius
+	m = sc.nextInt();
+	double fahrenheit;
+	System.out.println(fahrenheit=((m*1.8)+32));
+	}
+
+}
